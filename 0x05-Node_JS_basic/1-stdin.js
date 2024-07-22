945784b85e093e08dc48bfb591b6aabb8e9255af
Leniors <mokualeeroy@gmail.com>
2024-07-22 19:59:27 +0300

    console

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,13 @@
-// Display the welcome message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Set up an event listener to read input from the standard input
-process.stdin.on('data', (data) => {
-  // Trim the input data to remove any extra whitespace
-  const name = data.toString().trim();
-  
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-  
-  // Display the closing message
-  console.log('This important software is now closing');
-  
-  // Exit the process
-  process.exit();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
