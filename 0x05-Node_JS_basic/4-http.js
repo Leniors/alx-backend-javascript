4b3d8e0de61064c90437df367538b1295c195245
Leniors <mokualeeroy@gmail.com>
2024-07-23 14:33:29 +0300

    http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,16 +3,12 @@ const http = require('http');
 
 // Create the HTTP server and assign it to the variable app
 const app = http.createServer((req, res) => {
-  res.statusCode = 200; // Set status code to 200 (OK)
-  res.setHeader('Content-Type', 'text/plain'); // Set content type to plain text
-  res.end('Hello Holberton School!'); // Send response and end connection
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-// Make the server listen on port 1245
 const PORT = 1245;
 app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`...`);
 });
-
-// Export the app variable
-module.exports = app;
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs').promises;
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello Holberton School!');
+    }
+    else if (req.url === '/students') {
+        path = process.argv[2];
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('This is the list of our students\n');
+
+        try {
+            const file = await fs.readFile(path, 'utf-8');
+            const lines = file.trim().split('\n');
+            const students = lines.slice(1).filter((line) => line);
+        
+            res.write(`Number of students: ${students.length}\n`);
+        
+            const fields = {};
+            students.forEach((student) => {
+              const details = student.split(',');
+              const field = (details[details.length - 1]).trim();
+              const firstName = details[0];
+        
+              if (!fields[field]) {
+                fields[field] = [];
+              }
+              fields[field].push(firstName);
+            });
+        
+            const fieldEntries = Object.entries(fields);
+            fieldEntries.forEach(([field, students], index) => {
+                const isLast = index === fieldEntries.length - 1;
+                res.write(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+                if (!isLast) {
+                res.write('\n');
+                }
+            });
+        }
+        catch (err) {
+            res.write('Cannot load the database');
+        }
+        res.end();
+    }
+    else {
+        res.statusCode = 404;
+        res.end('Not Found');
+    }
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+    console.log(`...`);
+});
